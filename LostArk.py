0f0744d1906eda1eb83270ac158e0e10e34a7a8e
Song Yong Seob <yongsoub9809@gmail.com>
2023-05-19 13:37:24 +0900

    원정대 목록 및 캐릭터 이미지 1페이지 배치 완료. 사이즈조절중

--- LostArk.py
+++ LostArk.py
@@ -1,6 +1,93 @@
+from tkinter import *
+from tkinter import font, PhotoImage
+import tkinter.ttk
+from PIL import Image
+from PIL import ImageTk
+from io import BytesIO
+
 from SearchEngine import *
 
-SearchEngine()
+search_engine = SearchEngine()
+
+class LostArk:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('HoningCalc')
+        self.window.geometry('800x600')
+        self.window.configure(bg = 'white')
+        self.fontstyle = font.Font(self.window, size=24, weight='bold', family='consolas')
+        self.fontstyle2 = font.Font(self.window, size=15, weight='bold', family='consolas')
+        self.initPages()
+        self.initPage1()
+
+
+        self.window.mainloop()
+
+    def initPages(self):
+        self.notebook = tkinter.ttk.Notebook(self.window, width=800, height=600)
+        self.notebook.pack()
+
+        self.page1 = Frame(self.window)
+        self.notebook.add(self.page1, text='원정대')
+        self.page2 = Frame(self.window)
+        self.notebook.add(self.page2, text='재련')
+        self.page3 = Frame(self.window)
+        self.notebook.add(self.page3, text='지도')
+
+    def initPage1Canvas(self):
+        self.notebook.add(self.page1, text='원정대')
+
+        self.canvas = Canvas(self.page1)                        # 캔버스 위젯 생성
+        self.scrollbar = Scrollbar(self.page1, orient="vertical",
+                                   command=self.canvas.yview)   # 스크롤바 생성, 방향은 수직, 커맨드는 캔버스의 yview로 설정
+
+        self.scrollable_frame = Frame(self.canvas)              # 캔버스 내부에 스크롤 가능한 프레임 생성
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")            # 스크롤 가능 영역 설정
+            )
+        )
+
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")  # 스크롤 가능한 프레임을 캔버스에 추가
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)  # 캔버스의 y스크롤 커맨드를 스크롤바 set으로 설정
+
+    def initPage1ButtonEntry(self):
+        self.entry = Entry(self.page1)  # Entry 위젯 생성, 부모는 스크롤 가능한 프레임이 아니라 page1 프레임
+        self.entry.place(x=570, y=4)
+        self.button = Button(self.page1, text="Search",
+                             command=self.search_raidTeam)  # Button 위젯 생성, 부모는 스크롤 가능한 프레임이 아니라 page1 프레임
+        self.button.place(x=720, y=0)
+        self.canvas.pack(side="left", fill="both", expand=True)  # 캔버스를 왼쪽에 팩, fill과 expand로 크기 조절
+        self.scrollbar.pack(side="right", fill="y")  # 스크롤바를 오른쪽에 팩, fill로 크기 조절
+
+    def DisplayRaidTeam(self):
+        for i, character in enumerate(search_engine.raidTeam_Info):
+            image_url = character['image']
+            response = requests.get(image_url)
+            img_data = response.content
+            img = Image.open(BytesIO(img_data))
+            img = img.resize((204, 236))
+            img = ImageTk.PhotoImage(img)
+
+            img_label = Label(self.scrollable_frame, image=img)
+            img_label.image = img
+            img_label.grid(row=i // 3, column=i % 3 + 1)  # 3개씩 나열
+
+    def initPage1(self):
+        self.initPage1Canvas()
+        self.initPage1ButtonEntry()
+
+
+    def search_raidTeam(self):
+        characterName = self.entry.get()
+        search_engine.SearchRaidTeam(characterName)
+        search_engine.AddCharacterImages()
+        self.DisplayRaidTeam()
+
+LostArk()
 
 
 
--- SearchEngine.py
+++ SearchEngine.py
@@ -6,14 +6,17 @@ class SearchEngine:
         self.JWT_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAyNDExNDEifQ.CnRxSAg-TY9NwH8bhQ1MpTJQqwp-OETq5GxdiX_KKeK1POuQaHBwsW_U9YQgfrLiHU_nUaYZbm3RftSxW_k16OR-1k_Rq-ru2Du6LKywpOSgBfRBJFNkSVCJkox1MEhCG3c11N1HEiOjvuVymR8ha0qt9T-G416A0zmiP3dXX5wcXM2Qz9xausJaXxoVI9day180FJbAMumA4SRWnFrHKmBsIkLBMCq6jEwF62dK7bm3g6S2Q2ZA8nXChVwVye6-BNHtFKY75q5F7XwIK9GHuZCIu_KdVd8dafn83ERny5R8g3Ves5vLIrGf2SJY-Xqp9hA6Ed5zStY0cD9BqLLRiA'
         self.urls = {
             'market_option': 'https://developer-lostark.game.onstove.com/markets/options',
-            'siblings': 'https://developer-lostark.game.onstove.com/characters/%EB%A6%B0%ED%8B%B0%EC%95%88/siblings',
+            'raid_team': 'https://developer-lostark.game.onstove.com/characters/%EB%A6%B0%ED%8B%B0%EC%95%88/siblings',
             'character' : 'https://developer-lostark.game.onstove.com/armories/characters/%EB%A6%B0%ED%8B%B0%EC%95%88',
-            'market': 'https://developer-lostark.game.onstove.com/markets/search' }
+            'market': 'https://developer-lostark.game.onstove.com/markets/search',
+            'profiles' : 'https://developer-lostark.game.onstove.com/armories/characters/%EB%A6%B0%ED%8B%B0%EC%95%88/profiles'
+        }
         self.headers = {
             "accept": "application/json",
             "authorization": "Bearer " +self.JWT_TOKEN,
             "Content-Type": "application/json" }
-        self.categoriesCode = {}
+        self.categoriesCode = {}        #'장비 상자' : 10100 등이 저장되어 있다.
+        self.raidTeam_Info = []         #캐릭터 이름, 아이템레벨, 이미지를 담고 있다.
 
         response = requests.get(self.urls['market_option'], headers=self.headers)
         options = response.json()
@@ -34,3 +37,28 @@ class SearchEngine:
                     self.categoriesCode[sub['CodeName']] = sub['Code']
 
         print(self.categoriesCode)
+
+    def SearchRaidTeam(self, character_name):
+        url = self.urls['raid_team'].replace('%EB%A6%B0%ED%8B%B0%EC%95%88', character_name)      # 캐릭터 이름을 url에 적용
+        response = requests.get(url, headers=self.headers)                                      # url에 대한 요청 수행
+
+        if response.status_code == 200:
+            data = response.json()
+            self.raidTeam_Info = [{'CharacterName': character['CharacterName'], 'ItemMaxLevel': character['ItemMaxLevel']}
+                                  for character in data]        # 캐릭터 이름과 아이템 최대 레벨만 저장하는 리스트 생성
+        else:
+            print(f"Request failed with status code {response.status_code}")
+
+    def AddCharacterImages(self):
+        for character in self.raidTeam_Info:
+            url = self.urls['profiles'].replace('%EB%A6%B0%ED%8B%B0%EC%95%88', character['CharacterName'])  # 캐릭터 이름을 url에 적용
+            response = requests.get(url, headers=self.headers)                                              # url에 대한 요청 수행
+
+            if response.status_code == 200:
+                data = response.json()
+                character['image'] = data['CharacterImage']        # 이미지 URL을 characterInfo에 추가
+            else:
+                print(f"Request failed with status code {response.status_code}")
+
+        print(self.raidTeam_Info)
+
