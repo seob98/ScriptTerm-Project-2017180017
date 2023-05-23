49bc2ed3cb83caa3ff6b7d206e767e7893a68e7b
Song Yong Seob <yongsoub9809@gmail.com>
2023-05-23 15:51:42 +0900

    원정대 배치 방식 추가 수정. 버그 방지 및 재검색 허용

--- LostArk.py
+++ LostArk.py
@@ -13,19 +13,20 @@ class LostArk:
     def __init__(self):
         self.window = Tk()
         self.window.title('HoningCalc')
-        self.window.geometry('800x600')
+        self.window.geometry('830x600')
         self.window.configure(bg = 'white')
         self.fontstyle = font.Font(self.window, size=24, weight='bold', family='consolas')
         self.fontstyle2 = font.Font(self.window, size=15, weight='bold', family='consolas')
         self.initPages()
         self.initPage1()
         self.currentSelectedCharacterLv = 0
+        self.characterSelectRaidoButtons = []
 
 
         self.window.mainloop()
 
     def initPages(self):
-        self.notebook = tkinter.ttk.Notebook(self.window, width=800, height=600)
+        self.notebook = tkinter.ttk.Notebook(self.window, width=830, height=600)
         self.notebook.pack()
 
         self.page1 = Frame(self.window)
@@ -51,14 +52,19 @@ class LostArk:
 
     def initPage1ButtonEntry(self):
         self.entry = Entry(self.page1)  # Entry 위젯 생성, 부모는 스크롤 가능한 프레임이 아니라 page1 프레임
-        self.entry.place(x=570, y=4)
+        self.entry.place(x=600, y=4)
         self.button = Button(self.page1, text="Search",
                              command=self.search_raidTeam)  # Button 위젯 생성, 부모는 스크롤 가능한 프레임이 아니라 page1 프레임
-        self.button.place(x=720, y=0)
+        self.button.place(x=750, y=0)
         self.canvas.pack(side="left", fill="both", expand=True)  # 캔버스를 왼쪽에 팩, fill과 expand로 크기 조절
         self.scrollbar.pack(side="right", fill="y")  # 스크롤바를 오른쪽에 팩, fill로 크기 조절
 
     def DisplayRaidTeam(self):
+        for radio_button in self.characterSelectRaidoButtons:  # destroy the existing radio buttons
+            radio_button.destroy()
+        self.characterSelectRaidoButtons = []  # reset the list
+        self.canvas.delete("all")
+
         for i, character in enumerate(search_engine.raidTeam_Info):
             image_url = character['image']
             if image_url is not None:  # Add check for None image_url
@@ -72,17 +78,20 @@ class LostArk:
                 img_label.image = img
 
                 start_y_position = 40  # 캐릭터 이미지 배치
-                x_position = (i % 3 + 1) * 800 / 3.5 - 210
-                y_position = start_y_position + (i // 3) * 280
+                x_position = (i % 3 + 1) * 800 / 3.6 - 210
+                y_position = start_y_position + (i // 3) * 270
                 self.canvas.create_window(x_position, y_position, window=img_label, anchor='nw')
 
-                radio_button = Radiobutton(self.page1, text=character['CharacterName'], variable=self.currentSelectedCharacterLv, value=character['ItemMaxLevel'],
-                                              command=lambda lv=character['ItemMaxLevel']: self.selectCharacter(lv))
-                radio_button.place(x=x_position + 70, y=y_position + 240)
+                radio_button = Radiobutton(self.canvas, text=character['CharacterName'],
+                                           variable=self.currentSelectedCharacterLv, value=character['ItemMaxLevel'],
+                                           command=lambda lv=character['ItemMaxLevel']: self.selectCharacter(lv))
+                radio_button.place(x=680, y=start_y_position + i * 20 + 10)
+                self.characterSelectRaidoButtons.append(radio_button)
 
-
-            else:
-                pass # 예외처리 : 캐릭터 레벨이 너무 낮아 캐릭터 이미지를 제공 안하는 경우
+                if i // 3 >= 2:  # From the 3rd row onwards
+                    self.scrollable_frame.update_idletasks()  # Force the frame to update
+                    new_height = self.scrollable_frame.winfo_height() + img_label.winfo_height() + radio_button.winfo_height()
+                    self.scrollable_frame.configure(height=new_height)  # Update the frame's height
 
     def initPage1(self):
         self.initPage1Canvas()
